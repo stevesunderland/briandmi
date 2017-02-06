25ee6207e331f4322e3cf50ec6a03d1677df5a86
Steve Sunderland <stevesunderland@gmail.com>
2017-02-06 14:28:56 -0800

    add mobile menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,23 +9,29 @@ var Site = {
 		if ( $(window).width() > 640 ) {
 			Site.scrollmagic();
 		}
+		Site.mobileMenu();
+	},
+	mobileMenu: function(){
+		$(document).on('click', '.menu-toggle', function(event){
+			event.preventDefault();
+			$('body').toggleClass('menu-open');
+			return false;
+		})
+		$(document).on('click', '.menu a', function(){
+			$('body').removeClass('menu-open');
+		})
 	},
 	scrollmagic: function() {
-		// init controller
 		var controller = new ScrollMagic.Controller();
 		var images = $('.parallax');
 		var windowHeight = $(window).height();
 
 		$.each(images, function(index, image){
-			// var tween = new TweenMax.fromTo(image, 1, { transform: 'translateY(12em)' }, { transform: 'translateY(-8em)' })
 			var tween = new TweenMax.fromTo(image, 1, { transform: 'translateY(-4em)' }, { transform: 'translateY(12em)' })
-
 			var scene = new ScrollMagic.Scene({
 					duration: windowHeight*2,
 					triggerElement: image,
 					triggerHook: 1,
-					// offset: 50*index
-					// offset: 0
 				})
 				.setTween( tween )
 				.addTo(controller)
